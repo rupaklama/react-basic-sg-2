bcf998a926a546d52b1811e4963e385f1566d4fd
Rupak Lama <rupaklamadev@gmail.com>
2023-08-25 13:03:59 -0400

    fix for custom hook

--- comps-basic/src/pages/CounterPage/CounterPage.js
+++ comps-basic/src/pages/CounterPage/CounterPage.js
@@ -9,23 +9,23 @@ const useCounter = initialCount => {
     console.log(count);
   }, [count]);
 
-  const handleClick = () => {
+  const increment = () => {
     setCount(count + 1);
   };
 
   // 2. In the hook, return an object the contains the variables the component needs
-  return { count, handleClick };
+  return { count, increment };
 };
 
 function CounterPage() {
   // 3. In the component, call your hook & destructure the props that component needs
-  const { count, handleClick } = useCounter(0);
+  const { count, increment } = useCounter(0);
 
   // 1. Move the state & related codes into the custom hook to reuse the code
   return (
     <div>
       <h1>Count is {count}</h1>
-      <Button onClick={handleClick}>Increment</Button>
+      <Button onClick={increment}>Increment</Button>
     </div>
   );
 }
